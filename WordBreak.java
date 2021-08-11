5fdd47218dda5c2f9707dec917fdd6e440888ae5
sOubHik <soubhik93@gmail.com>
2021-08-11 10:05:55 +0530

    Create WordBreak.java

---
+++ WordBreak.java
@@ -0,0 +1,41 @@
+/*
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+ 
+
+*/
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] mem = new boolean[s.length() + 1];
+        mem[0] = true;
+        
+        for(int i = 1; i<= s.length(); i++) {
+            for(int j = i -1; j >=0;j--) {
+                mem[i] = mem[j] && wordDict.contains(s.substring(j, i));
+                if(mem[i]) break;
+            }
+        }
+        
+        return mem[s.length()];
+    }
+}
